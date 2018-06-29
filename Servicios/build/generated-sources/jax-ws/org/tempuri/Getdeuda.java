d5a1a04a2859d1ffef58d523b290793b1d18ba32
anidani <dany14nina@gmail.com>
2018-06-28 23:43:12 -0400

    Primera

---
+++ Servicios/build/generated-sources/jax-ws/org/tempuri/Getdeuda.java
@@ -0,0 +1,54 @@
+
+package org.tempuri;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="idc" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "idc"
+})
+@XmlRootElement(name = "getdeuda")
+public class Getdeuda {
+
+    protected int idc;
+
+    /**
+     * Obtiene el valor de la propiedad idc.
+     * 
+     */
+    public int getIdc() {
+        return idc;
+    }
+
+    /**
+     * Define el valor de la propiedad idc.
+     * 
+     */
+    public void setIdc(int value) {
+        this.idc = value;
+    }
+
+}
